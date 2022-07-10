197839e46a6811467f5fbd678c910763104038ba
Hieu Nguyen <nguyenluongtrunghieu1995@gmail.com>
2022-07-10 18:35:46 +0700

    Implement mongoose

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-// Middlewares
-const logger = require('./middlewares/logger')
-// Route files
-const bootcamps = require('./routes/bootcamps')
 
 // Load config vars
 dotenv.config({ path: './config/config.env' })
+
+const connectDB = require('./config/db')
+connectDB()
+
+// Middlewares
+// const logger = require('./middlewares/logger')
+// Route files
+const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
@@ -24,7 +28,13 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
+
+// Handle unhandle promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  server.close(() => process.exit(1))
+})
