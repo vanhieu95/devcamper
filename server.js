ff6f80271b14d0d8687cd520b152727a907267ba
Hieu Nguyen <nguyenluongtrunghieu1995@gmail.com>
2022-07-10 18:38:42 +0700

    Implement console colors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors');
 
 // Load config vars
 dotenv.config({ path: './config/config.env' })
@@ -30,11 +31,11 @@ const PORT = process.env.PORT || 5000
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
 
 // Handle unhandle promise rejection
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`)
+  console.log(`Error: ${err.message}`.red)
   server.close(() => process.exit(1))
 })
