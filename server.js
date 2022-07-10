dd7a094797f13a67cd254dde0230e641d136939e
Hieu Nguyen <nguyenluongtrunghieu1995@gmail.com>
2022-07-10 12:08:02 +0700

    Implement Logger  middleware

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const dotenv = require('dotenv')
-
+const morgan = require('morgan')
+// Middlewares
+const logger = require('./middlewares/logger')
 // Route files
 const bootcamps = require('./routes/bootcamps')
 
 // Load config vars
 dotenv.config({ path: './config/config.env' })
 
-const app = express() 
+const app = express()
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// Mount middleware
+// app.use(logger)
 
 // Mount routes
-app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
